dece463d7763e88b8c8c1a052974ee225331fccd
msgilligan <devnull@localhost>
2011-08-31 20:38:06 -0700

    Get appname and version from application.properties, etc

--- cloudbees-deploy.sh
+++ cloudbees-deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+APPNAME=`awk -F'=' '/app.name/ { print $2 }' application.properties | tr -d '\r\n'`
+APPVERSION=`awk -F'=' '/app.version/ { print $2 }' application.properties | tr -d '\r\n'`
+CBACCT=cmt
+CBAPP=iui
+echo "Building WAR for ${APPNAME} version ${APPVERSION}..."
 grails prod war
-grails bees-app-deploy cmt/iui v0.1 target/iuijs-0.1.war
+echo "Deploying ${APPNAME}-${APPVERSION}.war to CloudBees..."
+#grails bees-app-deploy cmt/iui v0.1 target/iuijs-0.1.war
+grails bees-app-deploy ${CBACCT}/${CBAPP} v${APPVERSION} target/${APPNAME}-${APPVERSION}.war
